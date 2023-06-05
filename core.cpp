ddaab8dd5e3febd4872849e73a6321ff1eb73515
Mika Schlup <mika.schlup@gmail.com>
2023-06-05 13:01:40 +0200

    Rewrite minimax

--- core.cpp
+++ core.cpp
@@ -111,8 +111,6 @@ struct Board {
                 // Get adjacent tiles
                 for (int x = -1; x <= 1; x++) {
                     for (int y = -1; y <= 1; y++) {
-                        if (x == 0 && y == 0) continue;
-
                         int newX = i + x;
                         int newY = j + y;
 
@@ -127,9 +125,10 @@ struct Board {
         }
         
         // Remove duplicates
+        //TODO: moves.sort();
         moves.unique();
 
-        /* DEBUG
+        /* DEBUG 
         Board newBoard(*this);
         for (Move move : moves) newBoard.array[move.placedTile.x][move.placedTile.y] = 'H';
         cout << newBoard.toString() << endl;
@@ -149,12 +148,14 @@ struct Board {
 
     // Make a move
     void makeMove(Move* move) {
-        // Place new tile
-        array[move->placedTile.y][move->placedTile.x] = isBlackTurn ? BLACK : WHITE;
+        if (move->placedTile.x != -1 && move->placedTile.y != -1) {
+            // Place new tile
+            array[move->placedTile.y][move->placedTile.x] = isBlackTurn ? BLACK : WHITE;
 
-        // Flip tiles
-        for (TilePosition tilePosition : move->flippedTiles) {
-            array[tilePosition.y][tilePosition.x] = isBlackTurn ? BLACK : WHITE;
+            // Flip tiles
+            for (TilePosition tilePosition : move->flippedTiles) {
+                array[tilePosition.y][tilePosition.x] = isBlackTurn ? BLACK : WHITE;
+            }
         }
 
         // Switch turn
@@ -162,17 +163,18 @@ struct Board {
     }
 
     // Evaluate the score for the current board
-    int evaluateScore(bool isBlack) {
+    // White is maximizing player
+    int evaluateScore() {
         int score = 0;
 
         // For each field
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 // If the tile is the same color as the player, add to score (positive), else subtract (negative)
-                if (array[i][j] == (isBlack ? BLACK : WHITE)) {
-                    score++;
-                } else if (array[i][j] == (isBlack ? WHITE : BLACK)) {
+                if (array[i][j] == BLACK) {
                     score--;
+                } else if (array[i][j] == WHITE) {
+                    score++;
                 }
             }
         }
@@ -196,34 +198,55 @@ struct Board {
 };
 
 // Minimax algorithm based on https://www.geeksforgeeks.org/minimax-algorithm-in-game-theory-set-4-alpha-beta-pruning/
-int _minimax(Board* board, int depth, bool isBlack, bool forBlack, int alpha = -INF, int beta = INF) {
+int _minimax(Board* board, int depth, int alpha = -INF, int beta = INF) {
     // If depth is reached return the score
-    if (depth == 0) return board->evaluateScore(forBlack);
+    if (depth == 0) return board->evaluateScore();
 
     list<Move> moves = board->getPossibleMoves();
     // If no moves are possible, game is over and return the score
-    if (moves.size() == 0) return board->evaluateScore(forBlack);
+    if (moves.size() == 0) {
+        board->isBlackTurn = !board->isBlackTurn;
+        moves = board->getPossibleMoves();
 
-    int maxEval = -INF;
-    for (Move move : moves) {
-        Board newBoard(*board);
-        newBoard.makeMove(&move);
-
-        // Recursively call minimax and get the best possible score
-        int eval = _minimax(&newBoard, depth - 1, !isBlack, forBlack, alpha, beta);
-        maxEval = max(maxEval, eval);
-
-        // Alpha-beta pruning
-        if (isBlack) alpha = max(alpha, eval);
-        else beta = min(beta, -eval);
-
-        if (beta <= alpha) break;
+        if (moves.size() == 0) return board->evaluateScore();
     }
 
-    return maxEval;
+    if (!board->isBlackTurn) {
+        int maxEval = -INF;
+        for (Move move : moves) {
+            Board newBoard(*board);
+            newBoard.makeMove(&move);
+
+            // Recursively call minimax and get the best possible score
+            int eval = _minimax(&newBoard, depth - 1, alpha, beta);
+            
+            // Alpha-beta pruning
+            maxEval = max(maxEval, eval);
+            alpha = max(alpha, maxEval);
+            if (beta <= alpha) break;
+        }
+
+        return maxEval;
+    } else {
+        int maxEval = INF;
+        for (Move move : moves) {
+            Board newBoard(*board);
+            newBoard.makeMove(&move);
+
+            // Recursively call minimax and get the best possible score
+            int eval = _minimax(&newBoard, depth - 1, alpha, beta);
+            
+            // Alpha-beta pruning
+            maxEval = min(maxEval, eval);
+            beta = min(beta, maxEval);
+            if (beta <= alpha) break;
+        }
+
+        return maxEval;
+    }
 }
 
-void _launchMinimax(Board board, Move move, int depth, int &bestEval, Move &bestMove, int &progress, int total) {
+void _launchMinimax(Board board, Move move, int depth, bool isMaximizingPlayer, int &bestEval, Move &bestMove, int &progress, int total) {
     chrono::steady_clock::time_point start = chrono::steady_clock::now();
 
     // Make move
@@ -231,8 +254,8 @@ void _launchMinimax(Board board, Move move, int depth, int &bestEval, Move &best
     newBoard.makeMove(&move);
 
     // Minimax and if best move yet, update best move
-    int eval = _minimax(&newBoard, depth, board.isBlackTurn, board.isBlackTurn);
-    if (eval > bestEval) {
+    int eval = _minimax(&newBoard, depth);
+    if ((isMaximizingPlayer && eval > bestEval) || (!isMaximizingPlayer && eval < bestEval)) {
         bestEval = eval;
         bestMove = move;
     }
@@ -248,13 +271,15 @@ Move getBestMove(Board* board, int depth) {
     list<Move> possibleMoves = board->getPossibleMoves();
     list<thread> threads;
 
+    bool isMaximizingPlayer = !board->isBlackTurn;
+
     int progress = 0;
-    int bestEval = -INF;
+    int bestEval = isMaximizingPlayer ? -INF : INF;
     Move bestMove;
 
     // Launch thread for each possible move
     for (Move move : possibleMoves) {
-        threads.push_back(thread(_launchMinimax, *board, move, depth, ref(bestEval), ref(bestMove), ref(progress), possibleMoves.size()));
+        threads.push_back(thread(_launchMinimax, *board, move, depth, isMaximizingPlayer, ref(bestEval), ref(bestMove), ref(progress), possibleMoves.size()));
     }
     // Wait for all threads to finish
     for (thread &thread : threads) thread.join();
@@ -271,7 +296,9 @@ int main() {
     long total = 0;
     long max = 0;
     int count = 0;
-    do {
+
+    int no_possible_move_count = 0;
+    while (no_possible_move_count < 2) {
         chrono::steady_clock::time_point start = chrono::steady_clock::now();
 
         move = getBestMove(&board, 6);
@@ -284,14 +311,17 @@ int main() {
 
         board.makeMove(&move);
         cout << board.toString() << endl;
-    } while (move.placedTile.x != -1);
+
+        if (move.placedTile.x == -1) no_possible_move_count++;
+        else no_possible_move_count = 0;
+    }
 
     cout << "Average: " << to_string(total / count) << "ms" << endl;
     cout << "Max: " << to_string(max) << "ms" << endl;
 
-    int score = board.evaluateScore(true);
-    if (score > 0) cout << "Black wins! (" + to_string(score) + ")" << endl;
-    else if (score < 0) cout << "White wins! (" + to_string(-score) + ")" << endl;
+    int score = board.evaluateScore();
+    if (score > 0) cout << "White wins! (" + to_string(score) + ")" << endl;
+    else if (score < 0) cout << "Black wins! (" + to_string(score) + ")" << endl;
     else cout << "Draw!" << endl;
 
     return 0;
